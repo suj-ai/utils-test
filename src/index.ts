befd003c64a42f9ccf96ff64b7c1c8934f640f54
sujai shanmugam <sujai.shanmugam@flyerssoft.com>
2022-10-11 18:40:10 +0530

    new function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import mixpanel from 'mixpanel-browser';
 
 let Analytics: any = {
-  token: '',
-  init: function(config: any) {
-    return mixpanel.init(this.token, config);
+  init: function(token: string, config: any) {
+    return mixpanel.init(token, config);
   },
   track: function(data: string, config: any) {
     return mixpanel.track(data, config);
