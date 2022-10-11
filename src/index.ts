27df0d0def7ecd1439949a0f15e67dfbd4c3fa4a
sujai shanmugam <sujai.shanmugam@flyerssoft.com>
2022-10-11 18:03:20 +0530

    new fix

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,13 @@
-import MixpanelBrowser, { Mixpanel } from 'mixpanel-browser';
-import { isNil } from 'lodash';
-import { MIXPANEL_TOKEN } from './Constants/constants';
+import mixpanel from 'mixpanel-browser';
 
-class Analytics {
-  private static _instance: Analytics;
-  private mixpanelApi: Mixpanel;
+let Analytics: any = {
+  token: '',
+  init: function(config: any) {
+    return mixpanel.init(this.token, config);
+  },
+  track: function(data: string, config: any) {
+    return mixpanel.track(data, config);
+  },
+};
 
-  private constructor() {
-    this.mixpanelApi = MixpanelBrowser;
-    this.mixpanelApi.init(
-      MIXPANEL_TOKEN,
-      {
-        debug: true,
-        ignore_dnt: true,
-      },
-      '',
-    );
-  }
-
-  public static get Instance(): Analytics {
-    return this._instance || (this._instance = new this());
-  }
-  async trackEvent(EventName: string, data?: object) {
-    return new Promise((resolve, reject) => {
-      try {
-        this.mixpanelApi.track(EventName, data, () => {
-          resolve(true);
-        });
-      } catch (error) {
-        reject(false);
-      }
-    });
-  }
-
-  async trackAnonymousEvent(eventName: string, data?: object) {
-    try {
-      const properties = isNil(data) ? { token: MIXPANEL_TOKEN } : { token: MIXPANEL_TOKEN, ...data };
-      const reqData = {
-        event: eventName,
-        properties: properties,
-      };
-      const body: any = new URLSearchParams();
-      body.append('data', JSON.stringify(reqData));
-      const options = {
-        method: 'POST',
-        body: body,
-      };
-      await fetch('https://api.mixpanel.com/track', options);
-    } catch (err) {
-      console.error('Error tracking anonymous mixpanel event', err);
-    }
-  }
-
-  reset() {
-    this.mixpanelApi.reset();
-  }
-}
-
-export default Analytics.Instance;
+export default Analytics;
